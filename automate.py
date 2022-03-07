ab9a3cd23e107f35b644c814c6320feba269a532
stevescilar <stevemuambi@gmail.com>
2022-03-07 18:36:02 +0300

    excel

--- automate.py
+++ automate.py
@@ -2,4 +2,13 @@ from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('marks.xlsx')
 ws = wb.active
-print(ws)
+# print(ws) -- print active sheet
+# print(ws['b2'].value)
+# ws['B3'].value = 'Kithungo Raha'
+
+# wb.save('marks.xlsx')
+# print(wb.sheetnames)
+# create a new sheet 
+wb.create_sheet('Manuel')
+wb.save('marks.xlsx')
+print(wb.sheetnames)
